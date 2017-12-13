cdf00ceb9cd353f61b8599f1e21aac46fd6e899e
Warrax <max.goudjo@hotmail.fr>
2017-12-13 16:39:25 +0100

    creation des entités room et roomcategorie

---
+++ src/AppBundle/Entity/Room.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Room
+ *
+ * @ORM\Table(name="room")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\RoomRepository")
+ */
+class Room
+{
+    /**
+     * @var
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\RoomCategory", inversedBy="rooms")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Room
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set category
+     *
+     * @param \AppBundle\Entity\RoomCategory $category
+     *
+     * @return Room
+     */
+    public function setCategory(\AppBundle\Entity\RoomCategory $category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \AppBundle\Entity\RoomCategory
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
