d7e2b53faeb1bd38aac9122500cbc81e7670299e
Arnaud Gillardin <arnaudgill89@live.fr>
2017-12-14 00:06:26 +0100

    badge +++

--- web/assets/js/autocomplete.js
+++ web/assets/js/autocomplete.js
@@ -16,12 +16,14 @@ $(".search-bar").keyup(function () {
                         currentCat = itemtypes[itemtype].icatName;
                         html += "<li class='catpppp'>" + itemtypes[itemtype].icatName + "</li>";
                     }
-                    html += "<li class='list-element-you-know'>" + itemtypes[itemtype].name + "</li>";
+                    html += "<li data-listid='" + itemtypes[itemtype].id + "' class='list-element-you-know'>" + itemtypes[itemtype].name + "</li>";
                 }
+
                 $('#autocomplete').html(html);
                 $('#autocomplete li').on('click', function () {
                     $('.search-bar').val($(this).text());
-                    var itemId = itemtypes[itemtype].id;
+                    var itemId = $(this).data('listid');
+                    console.log(itemId);
                     if ($('#tab-' + itemId).length === 0) {
                         elm = $("<li class=\"tab-pane room-item\" id=\"tab-" + itemId + "\">" + $(this).text() + "<span class='badge'>1</span></li>")
                         $('.room-items').append(elm);
@@ -30,8 +32,6 @@ $(".search-bar").keyup(function () {
                         span = $('#tab-' + itemId + " span:last-child");
                         count = parseInt(span.text()) + 1;
                         span.text(count);
-                        // $('#tab-' + itemId + "span:last-child").text('hello');
-                        console.log(count);
                     }
 
                 });
