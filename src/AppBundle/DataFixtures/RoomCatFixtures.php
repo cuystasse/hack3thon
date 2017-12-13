92fb2b4fca895493c4d51ba87fb56552651fdafc
Arnaud Gillardin <arnaudgill89@live.fr>
2017-12-13 17:18:06 +0100

    add fixtures base

---
+++ src/AppBundle/DataFixtures/RoomCatFixtures.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder8
+ * Date: 13/12/17
+ * Time: 17:11
+ */
+
+namespace AppBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class RoomCategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $tab = [
+            'Cuisine',
+            'Salon',
+            'Chambre',
+            'Salle de bain',
+            'Garage',
+        ];
+
+        foreach ($tab as $name) {
+            $roomCat = new RoomCategory();
+        }
+    }
+}
