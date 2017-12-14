09f7620c9ce3e51e1d20041bcc52185f70e9f5e7
Arnaud Gillardin <arnaudgill89@live.fr>
2017-12-14 15:34:13 +0100

    fix some trucs

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -3,21 +3,25 @@ function openRoom(evt, roomName) {
     var i, roomitems, roomlink;
 
     // Get all elements with class="room-items" and hide them
-    roomitems = document.getElementsByClassName("room-items");
-    for (i = 0; i < roomitems.length; i++) {
-        roomitems[i].style.display = "none";
-    }
+    // roomitems = document.getElementsByClassName("room-items");
+    // for (i = 0; i < roomitems.length; i++) {
+    //     roomitems[i].style.display = "none";
+    // }
+    $('.room-items').hide();
 
     // Get all elements with class="room-link" and remove the class "active"
-    roomlink = document.getElementsByClassName("room-link");
-    for (i = 0; i < roomlink.length; i++) {
-        roomlink[i].className = roomlink[i].className.replace(" active", "");
-    }
+    // roomlink = document.getElementsByClassName("room-link");
+    // for (i = 0; i < roomlink.length; i++) {
+    //     roomlink[i].className = roomlink[i].className.replace(" active", "");
+    // }
+    $('.room-link').removeClass('active');
 
     // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(roomName).style.display = "block";
+    // $('#roomName').show();
     evt.currentTarget.className += " active";
 
 }
 
-    document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen").click();
+// $('#defaultOpen').trigger("click");
