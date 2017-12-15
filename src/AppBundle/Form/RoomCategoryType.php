813f0bf7d5f19bad64a43700807ab050f95b14ca
Warrax <max.goudjo@hotmail.fr>
2017-12-15 13:41:39 +0100

    reglage erreur

--- src/AppBundle/Form/RoomCategoryType.php
+++ src/AppBundle/Form/RoomCategoryType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,7 +14,10 @@ class RoomCategoryType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('itemTypes');
+        $builder->add('name')->add('itemTypes', EntityType::class,[
+            "class" => \AppBundle\Entity\ItemType::class,
+            "choice_label" => "getName",
+        ]);
     }
     
     /**
